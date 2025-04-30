4a7e5836d0554c87b3cd85370e73b891d68452d1
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-04-29 21:52:38 -0400

    `map()`

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,7 @@ quo_is_named_function <- function(quosure) {
 map2 <- function(.x, .y, .f, ...) {
   mapply(.f, .x, .y, MoreArgs = list(...), SIMPLIFY = FALSE)
 }
+
+map <- function(.x, .f, ...) {
+  lapply(X = .x, FUN = .f, ...)
+}
